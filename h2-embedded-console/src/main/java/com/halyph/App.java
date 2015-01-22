a976ac557c4d8371a19a05c831c8e9906c9b36fc
Orest Ivasiv <halyph@gmail.com>
2015-01-22 23:01:39 +0200

    [h2-embedded-console] Code cleanup

--- h2-embedded-console/src/main/java/com/halyph/App.java
+++ h2-embedded-console/src/main/java/com/halyph/App.java
@@ -10,37 +10,36 @@ public class App {
     private static final String DBNAME = "mytest";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
-// open the in-memory database within a VM
+        // open the in-memory database within a VM
 
-        Class.forName("org.h2.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:h2:mem:" + DBNAME, "sa", "sa");
+        Class.forName("org.h2.Driver"); // (1)
+        Connection conn = DriverManager.getConnection("jdbc:h2:mem:" + DBNAME, "sa", "sa"); // (2)
         // username:password are very important and must be used for connecting via H2 Console
 
-        Statement stat = conn.createStatement();
-
-        // this line would initialize the database
-        // from the SQL script file 'init.sql'
-        // stat.execute("runscript from 'init.sql'");
-
+        Statement stat = conn.createStatement(); // (3)
         stat.executeUpdate("create table mytbl(id int primary key, name varchar(255))");
         stat.executeUpdate("insert into mytbl values(1, 'Hello')");
         stat.executeUpdate("insert into mytbl values(2, 'World')");
+
+        // Verify that sample data was really inserted
         ResultSet rs = stat.executeQuery("select * from mytbl");
         System.out.println("ResultSet output:");
         while (rs.next()) {
-            System.out.println(rs.getString("name"));
+            System.out.println("> " + rs.getString("name"));
         }
 
         // start a TCP server
-        Server server = Server.createTcpServer().start();
+        Server server = Server.createTcpServer().start(); // (4)
         // .. use in embedded mode ..
 
         // or use it from another process:
         System.out.println("Server started and connection is open.");
         System.out.println("URL: jdbc:h2:" + server.getURL() + "/mem:" + DBNAME);
 
-        // now start the H2 Console here or in another process using
-        // java org.h2.tools.Console -web -browser
+        System.out.println("\n");
+        System.out.println(
+                "now start the H2 Console in another process using:\n" +
+                "$ cd h2/bin; java -cp h2-1.4.185.jar org.h2.tools.Console -web -browser");
 
         System.out.println("Press [Enter] to stop.");
         System.in.read();
